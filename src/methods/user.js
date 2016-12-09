1a262e739f45e114115ffce2ec187aee5a37aeec
youxingzhi@qq.com <woshiyxz00>
2016-12-09 11:28:20 +0800

    实现用户基本功能

--- src/methods/user.js
+++ src/methods/user.js
@@ -30,6 +30,12 @@ module.exports = function (done) {
 
   });
 
+  $.method('user.get').check({
+    _id: {validate: (v) => validator.isMongoId(v)},
+    name: {validate: (v) => validator.isLength(v, {min: 4, max: 20}) && /^[a-zA-Z]/.test(v)},
+    email: {validator: (v) => validator.isEmail(v)},
+  });
+
   $.method('user.get').register(async function (params, callback) {
     const query = {};
     if (params._id) {
@@ -41,6 +47,40 @@ module.exports = function (done) {
     } else {
       return callback(new Error('missing parameter _id|name|email'));
     }
+
+    $.model.User.findOne(query, callback);
+  });
+
+  $.method('user.update').check({
+    _id: {validate: (v) => validator.isMongoId(v)},
+    name: {validate: (v) => validator.isLength(v, {min: 4, max: 20}) && /^[a-zA-Z]/.test(v)},
+    email: {validator: (v) => validator.isEmail(v)},
+  });
+
+  $.method('user.update').register(async function (params, callback) {
+    const user = await $.method('user.get').call(params);
+    if (!user) {
+      return callback(new Error('user does not exists'));
+    }
+
+    const update = {};
+    if (params.name && user.name !== params.name) {
+      update.name = params.name;
+    }
+    if (params.email && user.email !== params.email) {
+      update.email = params.email;
+    }
+    if (params.password) {
+      update.password = params.password;
+    }
+    if (params.nickname) {
+      update.nickname = params.nickname;
+    }
+    if (params.about) {
+      update.about = params.about;
+    }
+
+    $.model.User.update({_id:user._id}, {$set: update}, callback);
   });
 
   done();
--- src/test.js
+++ src/test.js
@@ -1,3 +1,12 @@
-$.method('user.add').call({
-  name: 'hello',
+// $.method('user.add').call({
+//   name: 'hello2',
+//   email: 'xxdddxx@qq.com',
+//   password: '123456',
+//   nickname: '测试一',
+//   about: '好腻害',
+// },console.log)
+
+
+$.method('user.update').call({
+  name: 'hello2'
 },console.log)
